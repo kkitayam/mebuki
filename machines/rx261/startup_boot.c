672dcbb515ebb4217d87582c57c2fb580293acc0
kkitayam <45088311+kkitayam@users.noreply.github.com>
2026-08-23 00:33:38 +0900

    fix: add .data and .bss initialization steps into startup routine

--- machines/rx261/startup_boot.c
+++ machines/rx261/startup_boot.c
@@ -5,12 +5,18 @@
 extern const uint8_t _isp_top[];
 extern const uint8_t _usp_top[];
 extern const uint8_t _text_load[];
+extern const uint8_t _data_load[];
 extern uint8_t _text[];
 extern uint8_t _etext[];
+extern uint8_t _data[];
+extern uint8_t _edata[];
+extern uint8_t _bss[];
+extern uint8_t _ebss[];
 
 extern void system_init(void) __attribute__((section(".text.reset")));
 extern int main(void);
 extern void *memcpy(void*,const void*,size_t);
+extern void *memset(void*,int,size_t);
 
 void Reset_Handler(void) __attribute__((section(".text.reset"),noinline));
 
@@ -19,9 +25,9 @@ void Reset_Handler(void)
     __MVTC(ISP, (uintptr_t)_isp_top);
     __MVTC(USP, (uintptr_t)_usp_top);
     system_init();
-    if ((uintptr_t)_text != (uintptr_t)_text_load) {
-        memcpy(_text, _text_load, _etext - _text);
-    }
+    memcpy(_text, _text_load, _etext - _text);
+    memcpy(_data, _data_load, _edata - _data);
+    memset(_bss, 0, _ebss - _bss);
     main();
 END:
     __WAIT();
